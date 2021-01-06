a5a2d5c0395576cf646d8aa348a547ce02d7060d
Drew Leske <drew.leske@computecanada.ca>
2021-01-06 14:21:43 -0800

    Add test key for scheduler

--- tests/dev.sql
+++ tests/dev.sql
@@ -1,7 +1,17 @@
-INSERT INTO clusters (id, name) VALUES ('testcluster', 'Test Cluster');
-INSERT INTO components (id, name, cluster, service) VALUES ('testcluster_detector', 'Detector', 'testcluster', 'detector');
+INSERT INTO clusters (id, name) 
+  VALUES ('testcluster', 'Test Cluster');
+INSERT INTO components (id, name, cluster, service)
+  VALUES ('testcluster_detector', 'Detector', 'testcluster', 'detector');
+INSERT INTO components (id, name, cluster, service)
+  VALUES ('testcluster_scheduler', 'Scheduler', 'testcluster', 'scheduler');
 -- test api keys
 -- secret is generated with
--- `dd if=/dev/urandom bs=1 count=46 2>/dev/null | base64`
+-- `dd if=/dev/urandom bs=1 count=48 2>/dev/null | base64`
 INSERT INTO apikeys (access, secret, component) VALUES (
-  'testapikey', 'WuHheVDysQQwdb+NK98w8EOHdiNUjLlz2Uxg/kIHqIGOek4DAmC5NCd2gZv7RQ==', 'testcluster_detector');
+  'testapikey',
+  'WuHheVDysQQwdb+NK98w8EOHdiNUjLlz2Uxg/kIHqIGOek4DAmC5NCd2gZv7RQ==',
+  'testcluster_detector');
+INSERT INTO apikeys (access, secret, component) VALUES (
+  'testapikey2',
+  't7zP2drVPhV9MdTypAsggshp2PQnpLS7ENVJq+P+BhAVJRMjmhInj6D7IP0tOcEz',
+  'testcluster_scheduler');
