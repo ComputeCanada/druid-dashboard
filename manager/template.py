f11321dae413afd3a0e1e625992dacbcd557ca7f
Drew Leske <drew.leske@computecanada.ca>
2021-03-27 19:17:34 -0700

    Improve handling of language-agnostic templates, ...
    Also:
    
    * Use proper exception for when template is not found
    * Use simple 'undefined' string if template variable not found
    * Remove stub code

--- manager/template.py
+++ manager/template.py
@@ -4,27 +4,12 @@
 import re
 from manager.db import get_db
 from manager.log import get_log
+from manager.exceptions import ResourceNotFound
 
 # regular expression for substituting template variables
 _re = r'%(?P<var>\w+)%'
 _rec = re.compile(_re)
 
-# pylint: disable=line-too-long
-_stubs = {
-  "other language follows": {
-    "en": "\n(La version française de ce message suit.)\n",
-    "fr": "\n(The English language version of this message follows.)\n"
-  },
-  "separator": "\n--------------------------------------\n",
-  "intro title": {
-    "en": "NOTICE: Your computations may be eligible for prioritised execution",
-    "fr": "AVIS: Vos calculs peuvent être éligibles pour une exécution prioritaire"
-  },
-  "intro": {
-    "en": "Hello %PREFERRED_NAME%,\n\nOngoing analysis of queued jobs on %CLUSTER% has shown that your project has a quantity of jobs that would benefit from a temporary escalation in priority.  Please let us know by replying to this message if you are interested.\n\nBest regards,\n%ANALYST%",
-    "fr": "Bonjour %PREFERRED_NAME%,\n\nAnalyse en cours des travaux en attente sur %CLUSTER% a montré que votre projet comporte une quantité d'emplois bénéficier d'une escalade temporaire en priorité. S'il vous plaît laissez-nous savoir par répondre à ce message si vous êtes intéressé.\n\nMeilleures salutations,\n%ANALYST%"
-  }
-}
 # ---------------------------------------------------------------------------
 #                                                               SQL queries
 # ---------------------------------------------------------------------------
@@ -42,19 +27,19 @@ SQL_GET = '''
 
 class Template:
 
-  def __init__(self, name, language=None):
+  def __init__(self, name, language=''):
     res = get_db().execute(SQL_GET, (name, language)).fetchone()
     if not res:
-      get_log().error(
-        "Could not load requested template (name=%s, language=%s) from database",
-        name, language)
-      raise "TODO: Better exception: Could not find requested template"
+      error = "Could not load requested template (name=%s, language=%s) from database" % \
+        (name, language)
+      get_log().error(error)
+      raise ResourceNotFound(error)
     self._content = res['content']
 
   def render(self, values=None):
     values = values or {}
     return re.sub(
       _rec,
-      lambda x: str(values.get(x['var'], "<<UNDEFINED('{}')>>".format(x['var']))),
+      lambda x: str(values.get(x['var'], "[undefined]")),
       self._content
     )
