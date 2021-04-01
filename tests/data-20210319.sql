3c744866abf5b0149607d8733ca3105503713e30
Drew Leske <drew.leske@computecanada.ca>
2021-04-01 09:53:33 -0700

    Remove comments with wrong syntax and duplicate record

--- tests/data-20210319.sql
+++ tests/data-20210319.sql
@@ -1,6 +1,3 @@
-#INSERT INTO clusters (id, name) VALUES ('testcluster', 'Test Cluster');
-#INSERT INTO clusters (id, name) VALUES ('testcluster2', 'Test Cluster 2');
-#INSERT INTO components (id, name, cluster, service) VALUES ('testcluster_detector', 'Detector', 'testcluster', 'detector');
 INSERT INTO components (id, name, cluster, service) VALUES ('testcluster_scheduler', 'Scheduler', 'testcluster', 'scheduler');
 INSERT INTO components (id, name, cluster, service) VALUES ('testcluster2_detector', 'Detector', 'testcluster2', 'detector');
 INSERT INTO components (id, name, cluster, service) VALUES ('testcluster2_scheduler', 'Scheduler', 'testcluster2', 'scheduler');
@@ -8,8 +5,6 @@ INSERT INTO components (id, name, cluster, service) VALUES ('testcluster2_schedu
 UPDATE apikeys SET access = 'testapikey_d', lastused = 20 WHERE access = 'testapikey';
 INSERT INTO apikeys (access, secret, component, lastused) VALUES (
   'testapikey_s', 'T3h5mwEk7mrVwxdon+s9blWhVh8zHDd7PVoUoWJsTf5Qd2EUie6I4pdBuyRykw==', 'testcluster_scheduler', 20);
-INSERT INTO apikeys (access, secret, component, lastused) VALUES (
-  'fakeyfakefake', 'ZoHCik4dOZm4VvKnkQUv9lcWydR8aH4bNCW2/fwxGGOfbj5SrBAY50nD3gNCIA==', 'testcluster_detector', 10);
 
 INSERT INTO apikeys (access, secret, component, lastused) VALUES (
   'testapikey2_d', 'rammarammadingdong', 'testcluster2_detector', 20);
