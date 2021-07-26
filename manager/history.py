4008fd972740fb97a1c90b74a7f776882fb3bc55
Drew Leske <drew.leske@computecanada.ca>
2021-07-26 12:20:29 -0700

    Store change values as JSON structure instead of strings
    * Use encapsulting "change" field instead of datum, was, now (format: JSON
      field; more flexible and allows JSON typing instead of everything being a
      string)
    * Use more specific exceptions than DatabaseException

--- manager/history.py
+++ manager/history.py
@@ -1,9 +1,10 @@
 # vi: set softtabstop=2 ts=2 sw=2 expandtab:
 # pylint: disable=raise-missing-from
 #
+import json
 from .db import get_db
 from .event import CaseEvent
-from .exceptions import DatabaseException
+from .exceptions import ResourceNotFound, ResourceNotCreated
 
 # ---------------------------------------------------------------------------
 #                                                               SQL queries
@@ -11,14 +12,14 @@ from .exceptions import DatabaseException
 
 SQL_CREATE_HISTORY = '''
   INSERT INTO history
-              (case_id, analyst, note, datum, was, now)
-  VALUES      (?, ?, ?, ?, ?, ?)
+              (case_id, analyst, note, change)
+  VALUES      (?, ?, ?, ?)
 '''
 
 SQL_CREATE_HISTORY_WITH_TIMESTAMP = '''
   INSERT INTO history
-              (case_id, analyst, note, timestamp, datum, was, now)
-  VALUES      (?, ?, ?, ?, ?, ?, ?)
+              (case_id, analyst, note, timestamp, change)
+  VALUES      (?, ?, ?, ?, ?)
 '''
 
 SQL_GET_HISTORY_BY_ID = '''
@@ -41,53 +42,59 @@ SQL_GET_HISTORY_BY_CASE = '''
 class History(CaseEvent):
 
   @classmethod
+  def _make_dict(cls, datum, was, now):
+    if datum is None:
+      return None
+    return {
+      'datum': datum,
+      'was': was,
+      'now': now
+    }
+
+  @classmethod
   def get_events(cls, caseID):
     res = get_db().execute(SQL_GET_HISTORY_BY_CASE, (caseID,)).fetchall()
     if not res:
       return None
-    return [
-      History(id=rec['id'], caseID=caseID,
-        analyst=rec['analyst'],
-        timestamp=rec['timestamp'],
-        text=rec['note'],
-        datum=rec['datum'],
-        was=rec['was'],
-        now=rec['now'])
-      for rec in res
-    ]
+    return [ History(record=rec) for rec in res ]
 
-  def __init__(self, id=None, caseID=None, analyst=None, timestamp=None,
+  def __init__(self, id=None, caseID=None, record=None, analyst=None, timestamp=None,
     text=None, datum=None, was=None, now=None):
 
     db = get_db()
 
     # if load by ID
-    if id and not caseID:
+    if id and not record:
       res = db.execute(SQL_GET_HISTORY_BY_ID, (id,)).fetchone()
       if not res:
-        raise DatabaseException('Could not retrieve record from history with ID {}'.format(id))
+        raise ResourceNotFound('Could not retrieve record from history with ID {}'.format(id))
       self._text = res['note']
-      self._datum = res['datum']
-      self._was = res['was']
-      self._now = res['now']
+      self._change = json.loads(res['change'])
       super().__init__(id, res['case_id'], res['analyst'], res['timestamp'])
 
+    elif record:
+
+      self._text = record['note']
+      self._change = json.loads(record['change'])
+      super().__init__(record['id'], record['case_id'], record['analyst'], record['timestamp'])
+
     else:
+
+      self._text = text
+      self._change = self._make_dict(datum, was, now)
 
       # if id is not defined, create new
       if id is None:
         if timestamp:
           id = db.insert_returning_id(SQL_CREATE_HISTORY_WITH_TIMESTAMP, (
-            caseID, analyst, text, timestamp, datum, was, now))
+            caseID, analyst, self._text, timestamp, json.dumps(self._change)
+          ))
         else:
-          id = db.insert_returning_id(SQL_CREATE_HISTORY, (caseID, analyst, text,
-            datum, was, now))
+          id = db.insert_returning_id(SQL_CREATE_HISTORY, (
+            caseID, analyst, self._text, json.dumps(self._change)
+          ))
         if not id:
-          raise DatabaseException('Could not create new record in history')
+          raise ResourceNotCreated('Could not create new record in history')
         db.commit()
 
-      self._text = text
-      self._datum = datum
-      self._was = was
-      self._now = now
       super().__init__(id, caseID, analyst, timestamp)
