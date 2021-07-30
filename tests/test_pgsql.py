dce35db1a9276ea090ef9f4e321b82ffc0c969ca
Drew Leske <drew.leske@computecanada.ca>
2021-07-30 08:39:13 -0700

    Remove seeding in unseeded app fixture

--- tests/test_pgsql.py
+++ tests/test_pgsql.py
@@ -61,15 +61,4 @@ def empty_app(request):
   with app.app_context():
     init_db()
 
-    # this fixture is for testing a brand-new auction site or one with no
-    # current activity, but minimal seeding is still required, such as for
-    # testing API keys
-    get_db().execute("""
-      INSERT INTO clusters (id, name) VALUES ('testcluster', 'Test Cluster');
-      INSERT INTO components (id, name, cluster, service) VALUES ('testcluster_detector', 'Detector', 'testcluster', 'detector');
-      INSERT INTO apikeys (access, secret, component)
-      VALUES ('testapikey', 'WuHheVDysQQwdb+NK98w8EOHdiNUjLlz2Uxg/kIHqIGOek4DAmC5NCd2gZv7RQ==', 'testcluster_detector')
-    """)
-    get_db().commit()
-
   yield app
