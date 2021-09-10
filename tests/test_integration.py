1a0f285b1583f49cb80665385010ad73199191a7
Drew Leske <drew.leske@computecanada.ca>
2021-09-09 22:13:57 -0700

    Remove missing/renamed test file

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -14,7 +14,6 @@ from tests_authentication import *
 from tests_bursts import *
 from tests_oldjobs import *
 from tests_dashboard import *
-from tests_ajax import *
 from tests.otrsstub import OtrsStub
 from manager import create_app
 from manager.db import get_db, init_db, seed_db
