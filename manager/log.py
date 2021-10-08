69af8837f84b80801f1c59d74eb8170185524d11
Drew Leske <drew.leske@computecanada.ca>
2021-10-07 17:41:27 -0700

    Specify package name when instantiating logger
    By doing this the app's logging is distinct from other modules and can be
    controlled independently.  This way we can set log level to DEBUG for the
    application but not see activate debugging on a dependent module's debug,
    such as PsycoPG.

--- manager/log.py
+++ manager/log.py
@@ -13,11 +13,11 @@ def get_log():
     # initialize logging
     # this should be on a formatter attached to a handler but simple for now
     logging.basicConfig(format='%(asctime)s %(levelname)-7s %(message)s',
-                        datefmt='%Y%m%d.%H%M%S',
-                        level=logging.DEBUG)
-    _logger = logging.getLogger()
+                        datefmt='%Y%m%d.%H%M%S')
+    _logger = logging.getLogger(__package__)
+    _logger.setLevel(logging.DEBUG)
 
-    _logger.debug("Initializing logger")
+    _logger.debug("Initializing logger for %s", __package__)
 
   return _logger
 
