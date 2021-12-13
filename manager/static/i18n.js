dc4846e767e99c4a9a23522ba1c3db9af26c08f5
Drew Leske <drew.leske@computecanada.ca>
2021-12-12 21:24:58 -0800

    Simplify i18n handling in Javascript
    Detect language on server side and pass that to templating.  Load language file
    based on that variable which is a simple lookup table of translation strings,
    and define a couple of basic functions to handle on-the-fly translations as
    well as translation of static DOM elements at load.
    
    Do not use JSON, which does not support comments; do not load language file in
    asynchronously because you need it right away.

--- manager/static/i18n.js
+++ manager/static/i18n.js
@@ -1,79 +1,38 @@
-// inspired in part by https://codeburst.io/translating-your-website-in-pure-javascript-98b9fa4ce427
 "use strict"
 
-class I18n {
-  constructor(languages) {
-
-    this._lang = null;
-    if (languages.includes(navigator.language.substr(0,2))) {
-      this._lang = navigator.language.substr(0,2);
-      this._langFull = navigator.language;
-    }
-    else {
-      // navigator.languages is experimental, though widely supported
-      // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/languages
-      for (var i = 0; i < navigator.languages.length; i++) {
-        if (languages.includes(navigator.languages[i].substr(0,2))) {
-          this._lang = navigator.languages[i].substr(0,2);
-          this._langFull = navigator.languages[i];
-          console.log(`Chose ${this._lang} as language from navigator.languages`);
-          break;
-        }
-      }
-    }
-
-    if (!this._lang) {
-      // best we can do is to assume Canadian English
-      this._lang = 'en';
-      this._langFull = 'en_CA';
-    }
-
-    // find all translatable elements
-    this._elements = document.querySelectorAll("[data-i18n]");
+function i18n() {
+  //return i18n_strings[i18n_key];
+  var message = arguments[0];
+  var translation = i18n_strings[message];
+  if (!translation) {
+    return("NOT DEFINED");
   }
 
-  load() {
-    fetch(`/static/i18n/${this._lang}.json`)
-      .then((res) => res.json())
-      .then((strings) => {
-        this.translate_static(strings);
-        this._strings = strings;
-      })
-      .catch(() => {
-        console.error(`Could not load ${this._lang}.json.`);
-      }
-    );
+  var args = arguments;
+  function replacer(match, offset, string) {
+    return args[offset];
+  }
+  if (arguments.length > 1) {
+    return translation.replaceAll(/\$(\d+)/g, replacer);
   }
 
-  translate_static(translations) {
-    this._elements.forEach((element) => {
-      var name = element.dataset.i18n;
-      var text = translations[name];
-      if (text) {
-        element.innerHTML = text;
-      } else {
-        element.innerHTML = "NOT DEFINED";
-      }
-    });
-  }
+  return translation;
+}
 
-  translate() {
-    var message = arguments[0];
-    console.log(`Let's find a translation for ${message}`);
-    var translation = this._strings[message];
-    if (!translation) {
-      return("NOT DEFINED");
-    }
+// convenience alias for familiarity
+var _ = i18n;
 
-    var args = arguments;
-    function replacer(match, offset, string) {
-      console.log(`match = ${match}, offset = ${offset}, string = ${string}`);
-      return args[offset];
-    }
-    if (arguments.length > 1) {
-      return translation.replaceAll(/\$(\d+)/g, replacer);
-    }
+function i18n_static() {
+  // find all translatable elements
+  var elements = document.querySelectorAll("[data-i18n]");
 
-    return translation;
-  }
+  elements.forEach((element) => {
+    var name = element.dataset.i18n;
+    var text = i18n_strings[name];
+    if (text) {
+      element.innerHTML = text;
+    } else {
+      element.innerHTML = "NOT DEFINED";
+    }
+  });
 }
---
+++ manager/static/i18n/en.js
@@ -0,0 +1,61 @@
+i18n_strings = {
+    "Cancel": "Cancel",
+    "Update": "Update",
+    "Create note": "Create note",
+    "Okay": "Okay",
+
+    // used for accordion headers, like "Bursts candidates - reported 2021-11-22 03:42 a.m."
+    "REPORT_HEADER": "$1 - reported $2",
+    "REJECT_DESC": "By rejecting this burst candidate you are indicating it's not of interest for bursting.",
+    "NO_REPORTS": "There are no current reports for this cluster.",
+    "RETRIEVING_CLUSTER_INFO": "Retrieving cluster info...",
+    "EVENT_UPDATE": "Updated from <strong>$1</strong> to <strong>$2</strong>",
+    "EVENT_CLEARED": "Cleared (was <strong>$1</strong>)",
+    "EVENT_SET": "Set to <strong>$1</strong>",
+    "HISTORY_TITLE": "Update to $1",
+    "NOTE_TITLE": "Note",
+    "RETRIEVING_CASE_INFO": "Retrieving case info...",
+    "FAILED_TO_RETRIEVE_CASE_INFO": "Failed to retrieve case info",
+    "RETRIEVING_EVENTS": "Retrieving events...",
+    "FAILED_TO_RETRIEVE_EVENTS": "Failed to retrieve events",
+    "CHOOSE_OUTREACH_TYPE": "Choose outreach type",
+    "NAME_PI": "$1 (PI)", 
+    "UPDATING": "Updating...",
+    "UPDATE_FAILED": "Failed to update",
+    "CREATING_NOTE": "Creating note...",
+    "FAILED_TO_CREATE_NOTE": "Failed to create note",
+    "STEALING_CASE": "Stealing case",
+    "FAILED_TO_STEAL_CASE": "Failed to steal case",
+    "CLAIMING_CASE": "Claiming case",
+    "FAILED_TO_CLAIM_CASE": "Failed to claim case",
+    "RETRIEVING_TEMPLATE": "Retrieving template...",
+    "UNABLE_TO_RETRIEVE_TEMPLATE": "Unable to retrieve template of type $1",
+    "CREATING_TICKET_FOR_USER": "Creating ticket for user $1...",
+    "CREATION_OF_TICKET_FAILED": "Creation of ticket failed",
+    "RELEASING_CASE": "Releasing case",
+    "FAILED_TO_RELEASE_CASE": "Failed to release case",
+    "CASE_HISTORY": "Case history",
+    "ABOUT": "About",
+    "ABOUT_CONTENT": `<p>Druid (<b>D</b>ashboard for <b>R</b>esearcher
+<b>U</b>sage <b>I</b>ssues <b>D</b>etermination) is a web application
+supporting the analysis of problematic usage patterns in our resources and
+followup communication with users.</p>
+
+<p><a href='https://git.computecanada.ca/frak/burst/manager'>View the
+source</a>.</p><p>Please <a
+href='https://git.computecanada.ca/frak/burst/manager/-/issues'>report any
+issues</a> you may find.</p>`,
+    "VERSION": "Version: $1",
+    "STEAL": "Steal",
+    "STEAL_CONTENT": `By stealing this case you indicate you will follow up
+with the analysis of this job set and with the research group, as necessary.`,
+    "CLAIM": "Claim",
+    "CLAIM_CONTENT": `By claiming this case you indicate you will follow up
+with the analysis of this job set and with the research group, as necessary.`,
+    "RELEASE": "Release",
+    "RELEASE_CONTENT": `By releasing this case you make it available to be
+claimed by another analyst.  This does not affect ownership of the associated
+ticket.`,
+    "ACTION": "Action",
+    
+}
